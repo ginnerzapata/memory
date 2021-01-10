59945642b0241e569bdf521293b6e964b54a9822
Ginner Zapata <ginnerzapata@gmail.com>
2021-01-10 06:07:31 -0300

    rules updated

--- src/components/Rules.js
+++ src/components/Rules.js
@@ -3,8 +3,8 @@ function Rules() {
     <section className="box" id="rules">
       <h2>Rules</h2>
       <p>
-        Click every pokemon just once. Can you catch all the 150 first
-        generation pokemons?
+        Prof. Oaks wants you to catch one pokemon of every kind. We don't want
+        them to go extinct, so if you catch one more than once, you loose.
       </p>
     </section>
   )
